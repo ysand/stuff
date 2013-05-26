399ae2fb5eb7c3d4fc93559796c6faff0e21878a
ysand <yngve.sandvik@gmail.com>
2013-05-26 21:57:39 +0300

    Update Ingrediens.h

--- Ingrediens.h
+++ Ingrediens.h
@@ -3,15 +3,27 @@
 class Ingrediens
 {
 public:
-Ingrediens(double id, std::string name, double grammes, double water, double kjoule, double fat, double cholesterol, 
+Ingrediens();
+
+Ingrediens(double id, std::string name, double water, double kjoule, double fat, double cholesterol, 
   					double carbohydrate,double fiber, double proteins, double vitA, double vitD, double vitE, double vitC);
 
 double getID();
+double getWater();
+double getKjoule();
+double getFat();
+double getCholesterol();
+double getCarbohydrate();
+double getFiber();
+double getProteins();
+double getVitA();
+double getVitD();
+double getVitE();
+double getVitC();
 
 private:
 	double id;
 	std::string name;
-	double grammes;
 	double water;
 	double kjoule;
 	double fat;
